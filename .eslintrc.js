c878b67370666a7fa897de07b8b51a7585b5d18e
Burak Kaplan <burak@kaplan.dev>
2019-12-24 15:11:55 +0300

    Configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "parserOptions": {
+      "ecmaVersion": 2019,
+      "sourceType": "module"
+  },
+  "env": {
+      "es6":true,
+      "browser": true
+  },
+  "plugins": [
+      "svelte3",
+      "prettier"
+  ],
+  "extends": ["prettier"],
+  "overrides": [
+      {
+          "files": ["**/*.svelte"],
+          "processor": "svelte3/svelte3"
+      }
+  ]
+};
