b3a51edb494b56f4ddf7fd18cf9fb7c94dea1a30
Burak Kaplan <burak@kaplan.dev>
2019-12-24 15:13:25 +0300

    Add @Services payth to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,10 @@ const pkg = require("./package.json");
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
 
-const alias = { svelte: path.resolve("node_modules", "svelte") };
+const alias = { 
+    svelte: path.resolve("node_modules", "svelte"),
+    '@Services': path.resolve(__dirname, 'src/services/')
+};
 const extensions = [".mjs", ".js", ".json", ".svelte", ".html"];
 const mainFields = ["svelte", "module", "browser", "main"];
 
