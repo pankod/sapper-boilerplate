988b3ded3ea63aaa05a8b6a595caa9d2f22a0e83
Burak Kaplan <burak@kaplan.dev>
2019-12-25 15:31:15 +0300

    Import dotenv insted of require

--- src/server.js
+++ src/server.js
@@ -3,8 +3,9 @@ import polka from "polka";
 import compression from "compression";
 import * as sapper from "@sapper/server";
 import devProxy from "./proxy";
+import dotenv from "dotenv";
 
-require('dotenv').config();
+dotenv.config();
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
