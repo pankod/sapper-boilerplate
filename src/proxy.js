ab71d0fece2e12f03535e5c563ea152d8ccc9d13
Burak Kaplan <burak@kaplan.dev>
2019-12-27 17:24:03 +0300

    Add proxy for "images"

--- src/proxy.js
+++ src/proxy.js
@@ -1,10 +1,18 @@
 import proxy from "http-proxy-middleware";
 
-const devProxy = proxy({
-    target: "https://api.nasa.gov",
-    pathRewrite: { "^/api": "" },
-    changeOrigin: true,
-    logLevel: 'debug'
-});
+const proxies = {
+    api: proxy({
+        target: "https://api.nasa.gov",
+        pathRewrite: { "^/api": "" },
+        changeOrigin: true,
+        logLevel: 'debug'
+    }),
+    images: proxy({
+        target: "https://images-api.nasa.gov",
+        pathRewrite: { "^/images-api": "" },
+        changeOrigin: true,
+        logLevel: 'debug'
+    }),
+};
 
-export default devProxy;
+export default proxies;
--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ import sirv from "sirv";
 import polka from "polka";
 import compression from "compression";
 import * as sapper from "@sapper/server";
-import devProxy from "./proxy";
+import proxy from "./proxy";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
 polka() // You can also use Express
-    .use('/api', devProxy)
+    .use('/api', proxy.api)
+    .use("/images-api", proxy.images)
     .use(compression({ threshold: 0 }), sirv("static", { dev }), sapper.middleware())
     .listen(PORT, err => {
         if (err) console.log("error", err);
