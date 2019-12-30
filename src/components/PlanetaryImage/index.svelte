c51fbc25e02a46fed96cd556bf487e39d808e219
Burak Kaplan <burak@kaplan.dev>
2019-12-30 17:38:20 +0300

    PlanetaryImage style

--- src/components/PlanetaryImage/index.svelte
+++ src/components/PlanetaryImage/index.svelte
@@ -67,74 +67,12 @@
     };
 </script>
 
-<style lang="scss">
-    .planetary {
-        margin-top: 70px;
-        width: 315px;
-        display: flex;
-        flex-flow: column;
-        justify-content: center;
-        align-items: center;
-    }
+<style src="./style.scss">
 
-    .description {
-        width: 100%;
-        margin-top: 5px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        text-align: center;
-    }
-
-    .copyright {
-        font-style: italic;
-        font-size: 14px;
-    }
-
-    .form {
-        width: 100%;
-        margin-bottom: 15px;
-        display: flex;
-        flex-direction: column;
-
-        input {
-            width: 100%;
-            padding-left: 5px;
-            margin-right: 10px;
-            border-radius: 5px;
-        }
-
-        div {
-            width: 100%;
-            display: flex;
-        }
-        div:first-child {
-            justify-content: space-between;
-            margin-bottom: 10px;
-        }
-
-        div:nth-child(2) {
-            button {
-                width: 100%;
-            }
-        }
-
-        button {
-            height: 23px;
-            background-color: #f9da2e;
-            cursor: pointer;
-            border: none;
-            border-radius: 5px;
-            font-size: 14px;
-            padding: 3px 6px;
-            font-weight: 700;
-            color: #2c3e50;
-        }
-    }
 </style>
 
 <div class="planetary">
-    <div class="form">
+    <div class="planetary__form">
         <div>
             <input type="text" bind:value={keyword} data-cy="SearchInput" />
             <button on:click={makeSearch} data-cy="SearchButton">Search</button>
@@ -147,9 +85,9 @@
         <div data-cy="StatusMessage">No results found for "{keyword}"</div>
     {:else if $imageUrl}
         <img alt={$imageTitle} src={$imageUrl} data-cy="PlanetaryImage" data-type={$imageType} />
-        <div class="description">
+        <div class="planetary__description">
             {$imageTitle}
-            <span class="copyright">{$copyright}</span>
+            <span class="planetary__description__copyright">{$copyright}</span>
         </div>
     {/if}
 </div>
